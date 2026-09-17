86a8998d582c2827ce50f9fb643560b5d5b0668d
Laserwaste <permataalkautsar1@gmail.com>
2026-09-17 20:00:34 +0700

    Update Program.cs

--- NilaisiswaApp/Program.cs
+++ NilaisiswaApp/Program.cs
@@ -2,3 +2,9 @@
 int nilai = 0;
 
 Console.Write("aplikasi Nilai Siswa: ");
+
+Console.Write("Massukan Nama: ");
+namasiswa = Console.ReadLine();
+
+Console.Write();
+nilaisiswa = int.Parse(Console.ReadLine());
